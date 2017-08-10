49369cc8884907a679ac8fe8005d785f97719233
Robert Rollner <robrollner@gmail.com>
2017-08-09 21:29:50 -0600

    adding js updates

---
+++ assets/javascript/jquery.js
@@ -0,0 +1,19 @@
+//identify variables
+var cpuRandomNumber = [];
+
+var win = 0;
+var loss = 0;
+
+var redCrystal = '';
+var blueCrystal = '';
+var yellowCrystal = '';
+var greenCrystal = '';
+
+var playerScore = '';
+
+//console.log(); 
+
+var cpuRandomNumber = cpuRandomNumber[randomIntFromInterval(19, 120)] {
+	return Math.floor(Math.random()*(120-19+1)+19);
+	console.log(cpuRandomNumber);
+}
