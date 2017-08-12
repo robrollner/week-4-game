3294b6c37403d253cd23d30abe4975f9eea6e76a
Robert Rollner <robrollner@gmail.com>
2017-08-12 15:23:24 -0600

    changing score image

--- assets/javascript/jquery.js
+++ assets/javascript/jquery.js
@@ -4,45 +4,54 @@ var cpuRandomNumber = '';
 var win = 0;
 var loss = 0;
 
-var redCrystal = '';
-var blueCrystal = '';
-var yellowCrystal = '';
-var greenCrystal = '';
-
-var playerScore = '';
+var redCrystal = 0;
+var blueCrystal = 0;
+var yellowCrystal = 0;
+var greenCrystal = 0;
 
 //console.log(); 
 
 //enter gameNumber generator
-	var gameNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-	cpuRandomNumber = gameNumber + cpuRandomNumber;
-	console.log(cpuRandomNumber);
-
+var gameNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+cpuRandomNumber = gameNumber + cpuRandomNumber;
+console.log(cpuRandomNumber);
+$('#cpuRandomNumber').html(gameNumber);
 // click me on crystals objects
 
-	var clickCrystals = document.getElementById('crystalButtons');
-		console.log(crystalButtons);
-
-	$('#crystalButtons').click(function(){
+var clickCrystals = document.getElementById('crystalButtons');
+console.log(crystalButtons);
 
 //Crystals
 
-	var redCrystalNumber = Math.floor(Math.random() * 12) + 1;
-	redCrystal = redCrystalNumber + redCrystal;
-	console.log(redCrystal);
+$('.redCrystal').click(function() {
 
-	var blueCrystalNumber = Math.floor(Math.random() * 12) + 1;
-	blueCrystal = blueCrystalNumber + blueCrystal;
-	console.log(blueCrystal);
-
-	var yellowCrystalNumber = Math.floor(Math.random() * 12) + 1;
-	yellowCrystal = yellowCrystalNumber +yellowCrystal;
-	console.log(yellowCrystal);
-
-	var greenCrystalNumber = Math.floor(Math.random() * 12) + 1;
-	greenCrystal = greenCrystalNumber +greenCrystal;
-	console.log(greenCrystal);
+    var redCrystalNumber = Math.floor(Math.random() * 12) + 1;
+    redCrystal = redCrystalNumber + redCrystal;
+    console.log(redCrystal);
 });
 
+$('.blueCrystal').click(function() {
 
+    var blueCrystalNumber = Math.floor(Math.random() * 12) + 1;
+    blueCrystal = blueCrystalNumber + blueCrystal;
+    console.log(blueCrystal);
+});
 
+$('.yellowCrystal').click(function() {
+
+    var yellowCrystalNumber = Math.floor(Math.random() * 12) + 1;
+    yellowCrystal = yellowCrystalNumber + yellowCrystal;
+    console.log(yellowCrystal);
+});
+
+$('.greenCrystal').click(function() {
+
+    var greenCrystalNumber = Math.floor(Math.random() * 12) + 1;
+    greenCrystal = greenCrystalNumber + greenCrystal;
+    console.log(greenCrystal);
+});
+
+// var playerScore = function() {
+//     redCrystal + blueCrystal + greenCrystal + yellowCrystal
+//     console.log(playerScore());
+// };
