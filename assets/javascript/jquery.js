01c289dc038189a0ef7b629bda3d5bf718f8487c
Robert Rollner <robrollner@gmail.com>
2017-08-12 09:57:15 -0600

    adding images

--- assets/javascript/jquery.js
+++ assets/javascript/jquery.js
@@ -1,5 +1,5 @@
 //identify variables
-var cpuRandomNumber = [];
+var cpuRandomNumber = '';
 
 var win = 0;
 var loss = 0;
@@ -13,7 +13,36 @@ var playerScore = '';
 
 //console.log(); 
 
-var cpuRandomNumber = cpuRandomNumber[randomIntFromInterval(19, 120)] {
-	return Math.floor(Math.random()*(120-19+1)+19);
+//enter gameNumber generator
+	var gameNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+	cpuRandomNumber = gameNumber + cpuRandomNumber;
 	console.log(cpuRandomNumber);
-}
+
+// click me on crystals objects
+
+	var clickCrystals = document.getElementById('crystalButtons');
+		console.log(crystalButtons);
+
+	$('#crystalButtons').click(function(){
+
+//Crystals
+
+	var redCrystalNumber = Math.floor(Math.random() * 12) + 1;
+	redCrystal = redCrystalNumber + redCrystal;
+	console.log(redCrystal);
+
+	var blueCrystalNumber = Math.floor(Math.random() * 12) + 1;
+	blueCrystal = blueCrystalNumber + blueCrystal;
+	console.log(blueCrystal);
+
+	var yellowCrystalNumber = Math.floor(Math.random() * 12) + 1;
+	yellowCrystal = yellowCrystalNumber +yellowCrystal;
+	console.log(yellowCrystal);
+
+	var greenCrystalNumber = Math.floor(Math.random() * 12) + 1;
+	greenCrystal = greenCrystalNumber +greenCrystal;
+	console.log(greenCrystal);
+});
+
+
+
