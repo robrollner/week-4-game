ad93a7afd246c03b5b646b30e195591e4b7d8e2c
Robert Rollner <robrollner@gmail.com>
2017-08-13 16:14:52 -0600

    updating working code

--- assets/javascript/jquery.js
+++ assets/javascript/jquery.js
@@ -1,41 +1,64 @@
-//identify variables
-var cpuRandomNumber = '';
+$(document).ready(function() {
 
-var win = 0;
-var loss = 0;
+    //identify variables
 
-var allCrystal = 0;
+    var cpuRandomNumber = 0;
+    var winCounter = 0;
+    var lossCounter = 0;
+    var allCrystal = 0;
 
 
-//console.log(); 
 
-//enter gameNumber generator
-var gameNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
-cpuRandomNumber = gameNumber + cpuRandomNumber;
-console.log(cpuRandomNumber);
-$('#cpuRandomNumber').html(gameNumber);
-// click me on crystals objects
-var playerScore = 0;
+    //enter gameNumber generator
 
-var clickCrystals = document.getElementById('crystalButtons');
-console.log(crystalButtons);
+    var gameNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+    cpuRandomNumber = gameNumber + cpuRandomNumber;
+    console.log(cpuRandomNumber);
+    $('#cpuRandomNumber').html(gameNumber);
 
-//Crystals
 
-$('#crystalButtons').click(function() {
+    // click me on crystals objects
 
-    var CrystalNumber = Math.floor(Math.random() * 12-1) + 1;
-    allCrystal = CrystalNumber + allCrystal;
-    playerScore += allCrystal;
-    console.log(playerScore);
-    if (playerScore === gameNumber){
-    	alert('You Win!');
-    	//$('#winCol').html('<p>win</p>');
-    	win = win + 1;
-    } else if (playerScore >= gameNumber){
-    	alert('You Lose!');
-    	// $('#loseCol').html('<p>loss</p>');    	
-    	loss = loss + 1;
-    }
+    var playerScore = 0;
+    $('#score').html(playerScore);
+    $('#winCol').html('<p>Wins: </p>' + winCounter++);
+    $('#loseCol').html('<p>Loss: </p>' + lossCounter++);
+    var clickCrystals = document.getElementById('crystalButtons');
+    console.log('Current value', crystalButtons);
+
+    //Crystals
+
+    $('#crystalButtons').click(function() {
+
+        var CrystalNumber = Math.floor(Math.random() * 12) + 1;
+        allCrystal = CrystalNumber + allCrystal;
+        playerScore += allCrystal;
+        console.log(playerScore);
+        $('#score').html(playerScore);
+        if (playerScore === cpuRandomNumber) {
+            $('#winCol').html('<p>Wins: </p>' + winCounter++);
+            alert('You Win!');
+            gameRestart();
+        } else if (playerScore >= cpuRandomNumber) {
+            $('#loseCol').html('<p>Loss: </p>' + lossCounter++);
+            alert('You Lose!');
+            gameRestart();
+        }
+
+        //Game ReStart
+        function gameRestart() {
+            var gameNumber = Math.floor(Math.random() * (120 - 19 + 1) + 19);
+            cpuRandomNumber = gameNumber + cpuRandomNumber;
+            console.log(cpuRandomNumber);
+            playerScore = 0;
+            allCrystal = 0;
+            $('#score').html(playerScore);
+            $('#cpuRandomNumber').html(gameNumber);
+            console.log(gameNumber + " cpuRandomNumber");
+            console.log(playerScore + " playerScore");
+
+        }
+
+    });
+
 });
-
